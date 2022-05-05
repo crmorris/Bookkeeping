e6bb53df4d019cf741e049250953caed3ecae2c3
crmorris <33641568+crmorris@users.noreply.github.com>
2022-05-05 03:39:06 -0500

    Created portion of view and JS for adding account

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Programs.Models;
+
+namespace Programs.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+
+    public AccountController(ILogger<AccountController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult AddAccount()
+    {
+        return View();
+    }
+
+    public IActionResult EditAccount()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
